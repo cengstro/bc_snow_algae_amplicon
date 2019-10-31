fdafad1a7bdbfcc8095171fa9a9cd32b6c02f219
Casey Engstrom <cengstro@sfu.ca>
2019-10-31 16:02:39 -0700

    updated read paths, add info whether sequenced or not

--- code/wrangle/tidy_field.R
+++ code/wrangle/tidy_field.R
@@ -6,12 +6,13 @@ library(here)
 
 
 field <- read_csv(here("data/01_raw/field_2018.csv"))
+seq <- read_csv(here("data/03_filtered/rbcl/filtered_cleaned_rbcl_rel_abund.csv"))
 
 head(field)
 
 
 # clean up a few small details
-field_1 <- field %>%
+field_added_other_category <- field %>%
   mutate(
     # lump some of the color snow categories
     color_snow = case_when(color_snow %in% c("white","yellow") ~ "other",
@@ -19,8 +20,17 @@ field_1 <- field %>%
                            is.na(color_snow) ~ "other",
                            TRUE ~ color_snow),
     # change NA habitat to other
-    habitat = if_else(is.na(habitat), "other", habitat)
-  )
+    habitat = if_else(is.na(habitat), "other", habitat))
+
+# add info whether the sample was sequenced or not
+samples_w_seq <- seq %>% 
+  distinct(sample_id) %>% 
+  pull()
+
+field_have_seq <- field_added_other_category %>% 
+  mutate(have_seq = if_else(sample_id %in% samples_w_seq, TRUE, FALSE))
+
+
 
 
 
@@ -29,36 +39,40 @@ field_1 <- field %>%
 # in the format B1.05 where B is mountain, 1 is the first visit, and 5 is the 5th sample collected that day
 
 # assign unique mountain_letter code to each field site
-field_2 <- field_1 %>% 
+field_mtn_letter <- field_have_seq %>% 
   mutate(mountain_letter = case_when(str_detect(sample_id, "sax") ~ "X",
                                      str_detect(sample_id, "sey") ~ "S",
                                      str_detect(sample_id, "sky") ~ "K",
                                      str_detect(sample_id, "stm") ~ "M",
                                      TRUE ~ str_sub(sample_id, 0,1) %>% toupper()))
-field_2 %>% select(mountain_letter, sample_id)
+field_mtn_letter %>% select(mountain_letter, sample_id)
 
 # which visit to the mountain (only 1 visit for most sites)
-visit_number <- field_2 %>%
+visit_number <- field_mtn_letter %>%
   distinct(mountain_letter, date) %>% 
   arrange(mountain_letter, date) %>% 
   group_split(mountain_letter) %>% 
   map_df(rownames_to_column, "visit_number") %>% 
   mutate(alias_1 = paste0(mountain_letter, visit_number))
 
-field_3 <- field_2 %>% 
+field_visit_num <- field_mtn_letter %>% 
   left_join(visit_number, by=c("mountain_letter", "date"))
-field_3 %>% distinct(sample_id, date, mountain_letter, visit_number, alias_1) %>% arrange(date) %>% view()
+field_visit_num %>% distinct(sample_id, date, mountain_letter, visit_number, alias_1) %>% arrange(date) %>% view()
 
 # for each moutain visit, unique sample ID
-field_4 <- field_3 %>% 
+field_sample_uid <- field_visit_num %>% 
   arrange(-elev_m) %>% # from high to low since thats usually the order I collected in
   group_split(alias_1) %>% 
   map_df(rownames_to_column, "uid") %>% 
   mutate(alias = paste0(alias_1, ".", uid))
-field_4 %>% distinct(sample_id, alias, date) %>% arrange(date) %>% view()
+field_sample_uid %>% distinct(sample_id, alias, date) %>% arrange(date) %>% view()
 
 # remove some of the unnessecary columns
-field_5 <- field_4 %>% 
+field_removed_extra_cols <- field_sample_uid %>% 
   select(sample_id, alias, date, lat, lon, elev_m, color_snow, habitat, substrate, depth_cm, dom_morph)
 
-write_csv(field_5, here("data/02_tidied/tidied_field.csv"))
+
+
+
+# write output ------------
+write_csv(field_removed_extra_cols, here("data/02_tidied/tidied_field.csv"))
