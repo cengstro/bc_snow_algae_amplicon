ac5aa09d9fa96018e724481b62db73be01f475b7
Casey Engstrom <cengstro@sfu.ca>
2020-01-10 23:36:23 -0800

    assembled supplementary materials, moved scripts generating supp figs to manuscript/supp/code

---
+++ manuscript/supp/code/cellcount_stacked_barplot.R
@@ -0,0 +1,37 @@
+# cell count pie charts
+
+library(tidyverse)
+library(here)
+
+
+cellct <- read_csv(here("data/02_tidied/tidied.cell_count.csv"))
+field <- read_csv(here("data/02_tidied/tidied_field.csv"))
+
+cellct <- cellct %>% 
+  left_join(field, by="sample_id")
+
+
+colors <- c("Chlainomonas rubra"="#ac71b5", #purple
+            "Sanguina nivaloides"="#ff534a", #red
+            # Chloromonas: pastel greens / blues / yellows
+            "Chloromonas krienitzii"="#ffa54c",
+            "Chloromonas cf. brevispina"="#80c786", "Chloromonas cf. nivalis"="#4aaf86", 
+            #                 lt green                            teal
+            "Other Chloromonas"="#4aaf53", "Chloromonas"="#4aaf53", 
+            # greyish light green
+            "Green cell"="#4aaf53", "Other"="grey60") #greys
+# oops, ordered them backwards, too lazy to rewrite
+colors <- colors %>% rev()
+
+
+cellct_plot <- ggplot(cellct, aes(x=alias %>% fct_reorder(elev_m), 
+                     y=percent_of_sample)) +
+    geom_bar(aes(fill=morpho_sp %>% fct_relevel(names(colors))), position="stack", stat="identity") +
+    theme_classic() +
+    labs(x="Sample ID", y="Relative abundance", fill="Cell morphology")+
+    scale_fill_manual(values = colors) +
+    theme(text = element_text(size=8),
+          axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.5))  
+cellct_plot
+
+ggsave(filename = here::here("manuscript/supp/output/fig_S4_cellct.tiff"), width = 300, height = 100, units="mm", dpi=300)
---
+++ manuscript/supp/code/tree.R
@@ -0,0 +1,80 @@
+# make tree for supp
+# 2020-01-10
+
+library(tidyverse)
+library(here)
+library(ape)
+library(ggtree)
+
+
+# ML phylogenetic tree (IQTree) of rbcL ASVs that appear in filtered relative abundance dataset (613 tips)
+tree <- read.tree(file=here::here("output/tree/rbcl/asv613.fasta.treefile")) 
+# Taxonomy tbl of each ASV (652; includes ASVs filtered out in samples not used in final analysis)
+taxonomy <- read_csv(here::here("data/02_tidied/tidied.rbcl_taxonomy.csv"))
+# OTU of each ASV
+otus <- read_csv(here::here("output/asv_otu_key.csv"))
+
+
+# Misc edits of tree annotation data
+
+# rename one of the OTUs (should take care of this upstream)
+otus <- otus %>% 
+  mutate(otu_name= otu_name %>% str_remove("muramotoi "))
+
+taxonomy <- taxonomy %>% 
+  mutate(assignment = assignment %>% str_remove("root"))
+
+
+
+
+# Plot tree ---------
+
+# re-root tree
+root <- taxonomy %>% 
+  filter(class=="Trebouxiophyceae") %>% 
+  head(1) %>% 
+  pull(asv_id)
+
+rbcl.tree <- rbcl.tree %>% root(outgroup = root, resolve.root=T)
+
+rbcl_ggtree <- ggtree(rbcl.tree) # ignore warning message 
+
+# format bayesian/bootstrap confidence for each node
+boots <- rbcl_ggtree$data %>% 
+  filter(isTip == FALSE) %>% 
+  separate(label, c("boot", "bayes"), sep = "/") %>% #split label into boots and bayes vals
+  filter(boot > 80) %>%
+  mutate(boot = round(as.numeric(boot), digits = 0),
+         insignificant_edge = if_else(boot > 80, "black", "red"))# round boot to nearest integer
+
+# format taxonomy, otu for each node
+assignment <- taxonomy %>% 
+  select(asv_id, assignment)
+annotation <- rbcl_ggtree$data %>% 
+  left_join(assignment, by=c("label"="asv_id")) %>% 
+  left_join(otus, by=c("label"="asv_id"))
+
+# specify colors
+rbcl_otu_colors <- c("Chlainomonas A"="#ac71b5",
+                     "Sanguina B"="#ff534a", 
+                      "Chloromonas krienitzii"="#ffa54c","Chloromonas C"="#87b1d4", "Chloromonas D"="#94cf92", "Chloromonas F"="#fdfd96", "Other Chloromonas"="#4aaf53", 
+                     "Trebouxiophyceae G"="#975c5c",
+                     "Unknown E"="grey60")
+
+
+# plot tree
+otu_tree <- rbcl_ggtree +
+  geom_text2(data=boots, aes(label=paste0(boot,";",bayes)),  vjust= 0.01, hjust = 1, size=2) +
+  geom_tippoint(data = annotation, aes(color= otu_name), alpha=1, size=3) + 
+  geom_tiplab(data = annotation, aes(label = assignment), size=1) +
+  theme(legend.position = c(0.2,0.7), 
+        legend.title = element_blank(), 
+        legend.key = element_blank()) +
+  guides(alpha="none") +
+  scale_color_manual(values= rbcl_otu_colors)
+
+otu_tree
+
+
+
+ggsave(filename=here::here("manuscript/supp/drafts/fig_S3_tree.pdf"), width = 200, height = 425, units="mm", dpi=300)
