e087a5cfdf2186323708b8e730d5ffccdab2def6
Casey Engstrom <cengstro@sfu.ca>
2020-01-11 22:37:00 -0800

    tweak figures

--- manuscript/supp/code/cellcount_stacked_barplot.R
+++ manuscript/supp/code/cellcount_stacked_barplot.R
@@ -4,8 +4,8 @@ library(tidyverse)
 library(here)
 
 
-cellct <- read_csv(here("data/02_tidied/tidied.cell_count.csv"))
-field <- read_csv(here("data/02_tidied/tidied_field.csv"))
+cellct <- read_csv(here::here("data/cellct/tidied_cellct.csv"))
+field <- read_csv(here::here("data/field/tidy_field.csv"))
 
 cellct <- cellct %>% 
   left_join(field, by="sample_id")
@@ -28,10 +28,10 @@ cellct_plot <- ggplot(cellct, aes(x=alias %>% fct_reorder(elev_m),
                      y=percent_of_sample)) +
     geom_bar(aes(fill=morpho_sp %>% fct_relevel(names(colors))), position="stack", stat="identity") +
     theme_classic() +
-    labs(x="Sample ID", y="Relative abundance", fill="Cell morphology")+
+    labs(x="Sample ID (in order of increasing elevation)", y="Relative abundance", fill="Cell morphology")+
     scale_fill_manual(values = colors) +
     theme(text = element_text(size=8),
           axis.text.x = element_text(angle = 90, hjust = 1, vjust=0.5))  
 cellct_plot
 
-ggsave(filename = here::here("manuscript/supp/output/fig_S4_cellct.tiff"), width = 300, height = 100, units="mm", dpi=300)
+# ggsave(filename = here::here("manuscript/supp/final/fig_S4_cellct.tiff"), width = 300, height = 100, units="mm", dpi=300)
--- manuscript/supp/code/tree.R
+++ manuscript/supp/code/tree.R
@@ -1,4 +1,4 @@
-# make tree for supp
+# Make rbcl ASV tree
 # 2020-01-10
 
 library(tidyverse)
@@ -13,6 +13,7 @@ tree <- read.tree(file=here::here("output/tree/rbcl/asv613.fasta.treefile"))
 taxonomy <- read_csv(here::here("data/02_tidied/tidied.rbcl_taxonomy.csv"))
 # OTU of each ASV
 otus <- read_csv(here::here("output/asv_otu_key.csv"))
+rel_abund <- read_csv("data/rel_abund/3_filter/rbcl/rbcl_rel_abund.csv")
 
 
 # Misc edits of tree annotation data
@@ -47,17 +48,23 @@ boots <- rbcl_ggtree$data %>%
   mutate(boot = round(as.numeric(boot), digits = 0),
          insignificant_edge = if_else(boot > 80, "black", "red"))# round boot to nearest integer
 
+# get rel abund for each asv
+rel_abund_asv <- rel_abund %>% 
+  group_by(asv_id) %>% 
+  summarise(rel_abund=sum(rel_abund))
+
 # format taxonomy, otu for each node
 assignment <- taxonomy %>% 
   select(asv_id, assignment)
 annotation <- rbcl_ggtree$data %>% 
   left_join(assignment, by=c("label"="asv_id")) %>% 
-  left_join(otus, by=c("label"="asv_id"))
+  left_join(otus, by=c("label"="asv_id")) %>% 
+  left_join(rel_abund_asv, by=c("label"="asv_id"))
 
 # specify colors
 rbcl_otu_colors <- c("Chlainomonas A"="#ac71b5",
                      "Sanguina B"="#ff534a", 
-                      "Chloromonas krienitzii"="#ffa54c","Chloromonas C"="#87b1d4", "Chloromonas D"="#94cf92", "Chloromonas F"="#fdfd96", "Other Chloromonas"="#4aaf53", 
+                      "Chloromonas krienitzii"="#ffa54c","Chloromonas muramotoi C"="#87b1d4", "Chloromonas D"="#94cf92", "Chloromonas F"="#fdfd96", "Other Chloromonas"="#4aaf53", 
                      "Trebouxiophyceae G"="#975c5c",
                      "Unknown E"="grey60")
 
@@ -65,16 +72,17 @@ rbcl_otu_colors <- c("Chlainomonas A"="#ac71b5",
 # plot tree
 otu_tree <- rbcl_ggtree +
   geom_text2(data=boots, aes(label=paste0(boot,";",bayes)),  vjust= 0.01, hjust = 1, size=2) +
-  geom_tippoint(data = annotation, aes(color= otu_name), alpha=1, size=3) + 
+  geom_tippoint(data = annotation, aes(color= otu_name, size=rel_abund), alpha=1) + 
   geom_tiplab(data = annotation, aes(label = assignment), size=1) +
   theme(legend.position = c(0.2,0.7), 
         legend.title = element_blank(), 
         legend.key = element_blank()) +
   guides(alpha="none") +
-  scale_color_manual(values= rbcl_otu_colors)
+  scale_color_manual(values= rbcl_otu_colors) +
+  guides(size="none",colour = guide_legend(override.aes = list(size=5)))
 
 otu_tree
 
 
 
-ggsave(filename=here::here("manuscript/supp/drafts/fig_S3_tree.pdf"), width = 200, height = 425, units="mm", dpi=300)
+ggsave(filename=here::here("manuscript/supp/intermediate/fig_S3_tree.pdf"), width = 200, height = 425, units="mm", dpi=300)
